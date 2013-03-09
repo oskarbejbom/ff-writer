6cd094b38dfae80fdd608e50fb6f0d4134581456
oskar <ottorius@gmail.com>
2013-03-09 09:26:18 +0100

    made current_week into an environment variable

--- db.rb
+++ db.rb
@@ -174,7 +174,7 @@ MongoMapper.connection = Mongo::Connection.new(host, port)
 MongoMapper.database = db_name
 MongoMapper.database.authenticate(db_name, pw)
 
-current_week = 29
+current_week = ENV['CURRENT_WEEK'].to_i
 
 loop do
 
