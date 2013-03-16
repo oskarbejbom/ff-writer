f26d1697ebbe6f4ff5f6801dacafcec0e96259c6
oskar <ottorius@gmail.com>
2013-03-16 11:21:30 +0100

    current week

--- db.rb
+++ db.rb
@@ -174,7 +174,7 @@ MongoMapper.connection = Mongo::Connection.new(host, port)
 MongoMapper.database = db_name
 MongoMapper.database.authenticate(db_name, pw)
 
-current_week = ENV['CURRENT_WEEK'].to_i
+current_week = 30
 
 loop do
 
