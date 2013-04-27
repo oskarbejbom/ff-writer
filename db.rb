2931cbbce44d9d35873cba5733ec606492308d1b
oskar <ottorius@gmail.com>
2013-04-27 13:23:06 +0200

    updated gw

--- db.rb
+++ db.rb
@@ -174,7 +174,7 @@ MongoMapper.connection = Mongo::Connection.new(host, port)
 MongoMapper.database = db_name
 MongoMapper.database.authenticate(db_name, pw)
 
-current_week = 34
+current_week = 35
 
 loop do
 
